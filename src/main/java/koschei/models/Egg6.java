f897d92024dc1964262eced7e0441397edb5ae34
exerusik <exe.rusik@gmail.com>
2024-03-25 18:22:24 +0300

    code style

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,15 +5,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private final Needle7 needle7;
+    @Autowired
+    private Needle7 needle;
 
     @Autowired
-    public Egg6(Needle7 needle7) {
-        this.needle7 = needle7;
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
+
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
     }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + needle7.toString();
+        return ", в яйце иголка " + needle.toString();
     }
 }
